0e4edb965a0b1b78cc8eaefeb990196e496d83e2
GaryThisSidee <144094016+GaryThisSidee@users.noreply.github.com>
2023-09-13 12:13:15 +0530

    build: added theme and some helpful packages

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;; Set up the visible-bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "FiraCode" :height 180)
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 180)
 
 (load-theme 'wombat)
 
@@ -30,6 +30,17 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; add line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers in some mode
+(dolist (mode `(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+  
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -53,20 +64,97 @@
         (global-set-key (kbd "C-s") 'swiper))
 
 (use-package counsel :ensure
-        :config
+     :config
         (global-set-key (kbd "M-x") 'counsel-M-x)
         (global-set-key (kbd "s-y") 'counsel-yank-pop)
-        (global-set-key (kbd "C-x C-f") 'counsel-find-file)) 
+        (global-set-key (kbd "C-x C-f") 'counsel-find-file))
+
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
 
 (use-package nyan-mode
+  :init (nyan-mode 1)
   :ensure t
   :hook (doom-modeline-mode . nyan-mode)
   :config
   (setq nyan-animate-nyancat t
         nyan-wavy-trail t)
-)
+  )
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+	 
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;;:ensure t
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;;:custom
+    ;;(corfu-auto )                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `global-corfu-modes'.
+  :init
+   (global-corfu-mode 1))
+
+;; Enable auto completion and configure quitting
+(setq corfu-auto 0.3
+      corfu-quit-no-match 'separator);; or t
+
+(use-package helpful
+  :ensure t
+    :custom
+    (counsel-describe-function-function #'helpful-callable)
+    (counsel-describe-variable-function #'helpful-variable)
+    :bind
+    ([remap describe-function] . counsel-describe-function)
+    ([remap describe-command] . helpful-command)
+    ([remap descibe-variable] . counsel-describe-variable)
+    ([remap describe-key] . helpful-key))
+
+(use-package nordic-night-theme
+  :ensure t
+  :config
+
+  ;; Use this for the darker version
+  ;; (load-theme 'nordic-midnight t)
+
+  (load-theme 'nordic-night t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(nordic-night-theme base16-theme timu-rouge-theme doom-themes helpful which-key rainbow-delimiters nyan-mode ivy-rich doom-modeline counsel corfu)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
