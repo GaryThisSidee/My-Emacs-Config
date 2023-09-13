fb75ac58ada3ed317e0e84881a70a1c240f546f3
GaryThisSidee <144094016+GaryThisSidee@users.noreply.github.com>
2023-09-13 16:41:35 +0530

    fix: added comments

--- init.el
+++ init.el
@@ -40,7 +40,8 @@
 		shell-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
-  
+
+;; gives suggestion in more detail
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -59,21 +60,24 @@
   :config
   (ivy-mode 1))
 
+;; basically like a grep
 (use-package swiper :ensure
         :config
         (global-set-key (kbd "C-s") 'swiper))
 
+;; counsel install
 (use-package counsel :ensure
      :config
         (global-set-key (kbd "M-x") 'counsel-M-x)
         (global-set-key (kbd "s-y") 'counsel-yank-pop)
         (global-set-key (kbd "C-x C-f") 'counsel-find-file))
 
-
+;; mode line 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
 
+;; nyan cat
 (use-package nyan-mode
   :init (nyan-mode 1)
   :ensure t
@@ -83,48 +87,33 @@
         nyan-wavy-trail t)
   )
 
+;; highlights paranthesis etc
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
+;; help with keys
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
 
+;; put extra details 
 (use-package ivy-rich
   :init (ivy-rich-mode 1))
-	 
+
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+;; Auto completion 
 (use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;;:ensure t
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;;:custom
-    ;;(corfu-auto )                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `global-corfu-modes'.
   :init
-   (global-corfu-mode 1))
+    (global-corfu-mode 1))
 
 ;; Enable auto completion and configure quitting
 (setq corfu-auto 0.3
       corfu-quit-no-match 'separator);; or t
 
+;; More better describe function and variabels
 (use-package helpful
   :ensure t
     :custom
@@ -136,10 +125,15 @@
     ([remap descibe-variable] . counsel-describe-variable)
     ([remap describe-key] . helpful-key))
 
-             
+ ;; doom themes
 (use-package doom-themes
   :ensure t
   :config
   (load-theme 'doom-laserwave t))
 
+;; install all the icons
+(use-package all-the-icons)
 
+
+
+       
